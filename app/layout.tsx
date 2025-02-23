1d1d3e830fb4b45de40b303cdeba93334a2099b3
Keth Lavene Abunda <laveniaketh@gmail.com>
2025-02-23 15:12:50 +0800

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-// import { ThemeProvider } from "./provider";
+import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,14 +19,14 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        {/* <ThemeProvider
+        <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
-          > */}
+          >
             {children}
-          {/* </ThemeProvider> */}
+          </ThemeProvider>
       </body>
     </html>
   );
