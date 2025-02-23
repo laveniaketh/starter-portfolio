bd35352a9e83f20620ded3ba4dcaf171efa7fb6f
Keth Lavene Abunda <laveniaketh@gmail.com>
2025-02-23 08:17:05 +0800

    added the utils

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+// import Experience from "@/components/Experience";
+// import Footer from "@/components/Footer";
+// import Grid from "@/components/Grid";
+// import Hero from "@/components/Hero";
+// import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto overflow-clip  sm:px-10 px-5 ">
+      <div className="max-w-7xl w-full">
+
+      </div>
     </main>
   );
 }
